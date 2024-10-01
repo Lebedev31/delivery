1f138655e4961acbf912e6fdaeb0f0268635bba2
Lebed <tireonreg@gmail.com>
2024-10-01 21:28:26 +0300

    Convert my-app from submodule to regular directory

---
+++ my-app/src/components/Header/Hamburger.tsx
@@ -0,0 +1,16 @@
+import './Hamburger.scss';
+
+function Hamburger() {
+  return (
+    <div className="wrapper__gamburger">
+      <div className="gamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div>Меню</div>
+    </div>
+  );
+}
+
+export default Hamburger;
